fda5703028bfd6b0eed4c3d2fb60146a8c69c0a9
Archana <archana13042004@gmail.com>
2025-04-23 13:49:07 +0530

    changes made

--- my-app/src/components/Hero/Hero.jsx
+++ my-app/src/components/Hero/Hero.jsx
@@ -5,10 +5,13 @@ import hand_icon from '../Assetss/hand_icon.png'
 import hero_image from '../Assetss/hero_image.png'
 const Hero = () => {
   return (
+    <>
+    <div className='text-bold text-blue-700'> my name is Archana</div>
     < div className='hero'>
         <div className="hero-left"> 
           <div className='text-white'>Hello users</div>
           <div className='text-bold'>welcome to clothhing site</div>
+          <div className='text-light'>Hello world.</div>
             <h2>NEW ARRIVALS ONLY</h2> 
             <div className="hero-hand-icon">
                 <p>new</p>
@@ -29,7 +32,7 @@ const Hero = () => {
       
 
       
-    </div>
+    </div></>
   )
 }
 
